b4a957148014822568cc6b54495631e16669aba7
Artur Gurgul <artur.gurgul@redge.com>
2023-12-19 08:24:24 +0100

    Add examples

---
+++ Sources/utils/Aliases.swift
@@ -0,0 +1,10 @@
+//
+//  Aliases.swift
+//
+//
+//  Created by Artur Gurgul on 19.12.23.
+//
+
+// Examples of aliases
+public typealias Point<T: Numeric> = (x: T, y: T)
+public typealias MyResult<T> = Result<T, Error>
